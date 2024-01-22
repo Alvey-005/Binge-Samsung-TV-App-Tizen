cf2b19897b21be3e8f0b28c13307626871e64a6e
Yeasin Zilani <sadafzillani98@gmail.com>
2024-01-22 16:48:09 +0600

    api call left

--- server/js/screen/login.js
+++ server/js/screen/login.js
@@ -62,6 +62,7 @@ window.login = {
 
   action: function (selected) {
     var options = document.getElementsByClassName(login.id + "-option");
+    console.log(options[selected].firstElementChild);
     if (selected == 1) {
       var phone = options[0].firstElementChild.value;
       if (phone.length < 10) {
